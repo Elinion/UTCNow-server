45677b378a626454a8b32c0680f00aa79011a5e5
alexandre abrantes <alexandre.abts@gmail.com>
2016-05-26 12:27:09 +0200

    Database connection snippet To test it: - create a local database named utcnow with one table named events (id, title) - start a mysql server (I use MAMP, make sure to change to port in the connection parameters if you use something else) - open localhost:8080 in your browser

--- web.js
+++ web.js
@@ -1,11 +1,51 @@
 /**
  * Created by alexandreabrantes on 5/19/16.
  */
+//var url = require('url');
+// var queryString = require('querystring');
 
-var http = require('http');
+// var server = http.createServer(function (req, res) {
+//     var params = queryString.parse(url.parse(req.url).query);
+//     res.writeHead(200, {"Content-Type": "text/plain"});
+//     if ('ticket' in params) {
+//         res.write('Ticket : ' + params['ticket']);
+//     }
+//     else {
+//         res.write('No CAS ticket received');
+//     }
+//     res.end();
+// });
+// server.listen(8080);
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Server base for UTCNow');
+var express = require('express');
+var mysql = require("mysql");
+
+var app = express();
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : '8889',
+    user     : 'alex',
+    database : 'utcnow'
 });
-server.listen(8080);
+
+connection.connect();
+
+var result = 'not found';
+
+connection.query('SELECT * FROM events', function(err, rows, fields) {
+    if (err) throw err;
+
+    result = JSON.stringify(rows);
+});
+
+connection.end();
+
+
+app.get('/', function (request, response) {
+    response.set('Content-Type', 'text/plain');
+    response.write(result);
+    response.end();
+});
+
+app.listen(8080);
