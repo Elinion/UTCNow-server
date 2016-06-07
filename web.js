267420df85ed93c0b8077dfcdcc8b4cd0a288cd3
kolopouri <baptiste.montange@gmail.com>
2016-06-07 21:04:49 +0200

    ajout Access-Control-Allow-Methods a l'header des requete http

--- web.js
+++ web.js
@@ -50,6 +50,7 @@ app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTION, TRACE');
     next();
 });
 
